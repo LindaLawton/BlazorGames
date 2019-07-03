9c5db02ba3d7a5786702a2c14b66758a0b3ba92a
Linda Lawton <laurly71@gmail.com>
2019-07-03 21:00:26 +0200

    fixes bug with paddle not moving to edge of screen if overflow pos

---
+++ src/Games.Client/Model/GameModel.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Games.Client.Pages;
+using Microsoft.AspNetCore.Components;
+
+namespace Games.Client.Model
+{
+    public class GameModel : ComponentBase
+    {
+
+        [Parameter]
+        public int Height { get; set; }
+        [Parameter]
+        public int Width { get; set; }
+
+        [Parameter]
+        public string Style { get; set; }
+
+        [Parameter]
+        public Dictionary<string, Player> Players { get; set; }
+
+        [Parameter]
+        public List<Ball> Balls { get; set; }  // fix issue with game with no balls.
+
+        [Parameter]
+        public Player Active { get; set; }  //TODO fix null issue for game with no paddles
+
+        public GameModel()
+        {
+            Style = "fill:rgb(220,220,220);stroke-width:3;stroke:rgb(0,0,0)";
+            Players = new Dictionary<string, Player>();
+            Balls = new List<Ball>();
+        }
+
+        public void AddBall(int radius, int speed, string style, Point currentPosition, Point nextPosition, Point previousPosition = null)
+        {
+            Balls.Add(new Ball() { Radius = radius, Speed = speed, Style = style, CurrentPosition = currentPosition, NextPosition = nextPosition, PreviousPosition = previousPosition });
+        }
+
+        public void MovePaddleLeft()
+        {
+            var pos = Active.Paddles.First().GetNextLeftPoint();
+
+            if (IsOnScreen(pos))
+                Active.Paddles.First().CurrentPosition = pos;
+            else
+                Active.Paddles.First().CurrentPosition = new Point(0, pos.Y);
+
+        }
+        public void MovePaddleRight()
+        {
+            var pos = Active.Paddles.First().GetNextRightPoint();
+            if (IsOnScreen(pos))
+                Active.Paddles.First().CurrentPosition = pos;
+            else 
+                Active.Paddles.First().CurrentPosition = new Point(Width - Active.Paddles.First().Width, pos.Y);
+        }
+
+        public void MovePaddleUp()
+        {
+            var pos = Active.Paddles.First().GetNextUpPoint();
+            if (IsOnScreen(pos))
+                Active.Paddles.First().CurrentPosition = pos;
+            else
+                Active.Paddles.First().CurrentPosition = new Point(pos.X, 0);
+        }
+        public void MovePaddleDown()
+        {
+            var pos = Active.Paddles.First().GetNextDownPoint();
+            if (IsOnScreen(pos))
+                Active.Paddles.First().CurrentPosition = pos;
+            else
+                Active.Paddles.First().CurrentPosition = new Point(pos.X, Height - Active.Paddles.First().Height);
+        }
+
+        private bool IsOnScreen(Point pos)
+        {
+            if (pos.X <= 0 || pos.X + Active.Paddles.First().Width >= Width)
+                return false;
+            if (pos.Y <= 0 || pos.Y + Active.Paddles.First().Height >= Height)
+                return false;
+
+            //TODO issue where paddle will over run if moved fully should pop to edge.
+
+            return true;
+        }
+    }
+}
--- src/Games.Client/Model/GamePannelModel.cs
+++ src/Games.Client/Model/GamePannelModel.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Games.Client.Pages;
 using Microsoft.AspNetCore.Components;
 
 namespace Games.Client.Model
@@ -18,91 +15,6 @@ namespace Games.Client.Model
         public void InvokeStartGameParentMethod()
         {
             StartGameParentMethod?.Invoke();
-        }
-    }
-
-    public class GameModel : ComponentBase
-    {
-
-        [Parameter]
-        public int Height { get; set; }
-        [Parameter]
-        public int Width { get; set; }
-
-        [Parameter]
-        public string Style { get; set; }
-
-        //[Parameter]
-        //public List<Paddle> Paddles { get; set; }
-
-        [Parameter]
-        public Dictionary<string, Player> Players { get; set; }
-
-        [Parameter]
-        public List<Ball> Balls { get; set; }  // fix issue with game with no balls.
-
-        [Parameter]
-        public Player Active { get; set; }  //TODO fix null issue for game with no paddles
-
-
-
-        public GameModel()
-        {
-            Style = "fill:rgb(220,220,220);stroke-width:3;stroke:rgb(0,0,0)";
-            Players = new Dictionary<string, Player>();
-            Balls = new List<Ball>();
-        }
-
-        //public void AddPaddle(int height, int width, int speed, string style, Point currentPosition, PaddleMovment paddleMovment, bool active = true)
-        //{
-        //    Paddles.Add(new Paddle() { Height = height, Width = width, Style = style, Speed = speed, CurrentPosition = currentPosition, PaddleMovment = paddleMovment });
-
-        //    if (active)
-        //        Active = Paddles.FirstOrDefault(p => p.Height.Equals(height) && p.Width.Equals(width) && p.Speed.Equals(speed) && p.Style.Equals(style) && p.CurrentPosition.Equals(currentPosition));
-        //}
-
-        public void AddBall(int radius, int speed, string style, Point currentPosition, Point nextPosition, Point previousPosition = null)
-        {
-            Balls.Add(new Ball() { Radius = radius, Speed = speed, Style = style, CurrentPosition = currentPosition, NextPosition = nextPosition, PreviousPosition = previousPosition });
-        }
-
-        public void MovePaddleLeft()
-        {
-            var pos = Active.Paddles.First().GetNextLeftPoint();
-
-            if (IsOnScreen(pos))
-                Active.Paddles.First().CurrentPosition = pos;
-
-        }
-        public void MovePaddleRight()
-        {
-            var pos = Active.Paddles.First().GetNextRightPoint();
-            if (IsOnScreen(pos))
-                Active.Paddles.First().CurrentPosition = pos;
-        }
-
-        public void MovePaddleUp()
-        {
-            var pos = Active.Paddles.First().GetNextUpPoint();
-            if (IsOnScreen(pos))
-                Active.Paddles.First().CurrentPosition = pos;
-        }
-        public void MovePaddleDown()
-        {
-            var pos = Active.Paddles.First().GetNextDownPoint();
-            if (IsOnScreen(pos))
-                Active.Paddles.First().CurrentPosition = pos;
-        }
-
-        private bool IsOnScreen(Point pos)
-        {
-            if (pos.X <= 0 || pos.X + Active.Paddles.First().Width >= Width)
-                return false;
-            if (pos.Y <= 0 || pos.Y + Active.Paddles.First().Height >= Height)
-                return false;
-
-
-            return true;
         }
     }
 }
